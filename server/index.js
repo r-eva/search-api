b0cd214b42a1e58072c61f2423b19d058408828a
regina <reginaevadewi@gmail.com>
2021-06-12 17:49:18 +0200

    first commit

--- server/index.js
+++ server/index.js
@@ -4,7 +4,13 @@ const cors = require('cors');
 const port = 3005
 
 app.use(cors());
+app.use(express.json())
 
 app.get('/', (req, res) => res.send('Hi there!'));
+
+/// Search Endpoints ////
+const { searchRouter } = require('../routers')
+
+app.use('/activities', searchRouter)
 
 app.listen(port, () => console.log(`Demo server listening on port ${port}!`))
